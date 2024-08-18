a76eff15443c7179f6764d5e9da80c34337c259c
Vikus7 <victor.rodriguez01@epn.edu.ec>
2024-08-18 03:23:35 -0500

    Proyecto 2 manifest solved

--- Proyecto/app/src/main/java/com/example/proyecto/MainActivity.kt
+++ Proyecto/app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.proyecto
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button  // Importa la clase Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Configura los insets para que los elementos de la UI no queden detrás de los system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Encuentra el botón por su ID y configura un OnClickListener para iniciar HomeActivity
+        val iniciarSesionButton = findViewById<Button>(R.id.IniciarSesion)
+        iniciarSesionButton.setOnClickListener {
+            // Crea un intent que señala hacia HomeActivity
+            val intent = Intent(this, Home::class.java)
+            // Inicia la actividad
+            startActivity(intent)
         }
     }
 }
